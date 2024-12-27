f5ca1f938113c7c81d7ec0d9ae787afcf795e1b6
Zach Daniel <zach@zachdaniel.dev>
2024-12-27 11:36:01 -0500

    chore: update ex_doc

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule AshSql.MixProject do
       {:simple_sat, "~> 0.1", only: [:dev, :test]},
       {:benchee, "~> 1.1", only: [:dev, :test]},
       {:git_ops, "~> 2.5", only: [:dev, :test]},
-      {:ex_doc, github: "elixir-lang/ex_doc", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.32", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.14", only: [:dev, :test]},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
