5b59420734ad08a23f1180b92f8e9922a1b29dbc
Zach Daniel <zach@zachdaniel.dev>
2025-05-06 09:45:19 -0400

    chore: update ash

--- mix.exs
+++ mix.exs
@@ -69,8 +69,7 @@ defmodule AshSql.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:ash, ash_version("~> 3.0 and >= 3.5.0")},
-      {:ash, ash_version(github: "ash-project/ash")},
+      {:ash, ash_version("~> 3.0 and >= 3.5.0")},
       {:ecto_sql, "~> 3.9"},
       {:ecto, "~> 3.9"},
       # dev/test dependencies
