a1145acb48e0c6c91de2b705a4a00594e02ff261
Steve Brambilla <steve@brambilla.ca>
2025-09-17 15:18:24 -0400

    improvement: support immutable version of ash_raise_error
    This improves support for Citus (distributed Postgres) where it only
    supports immutable functions within CASE statements. A row-dependent
    token is passed in as an ignored argument to prevent the query planner
    from constant-folding the ash_raise_error_immutable call.
    
    This is opt-in only. The repo config must have an :immutable_expr_error?
    entry set to true (supported by ash_postgres currently).

--- lib/expr.ex
+++ lib/expr.ex
@@ -2080,29 +2080,51 @@ defmodule AshSql.Expr do
         {Jason.encode!(%{exception: inspect(exception), input: Map.new(input)}), acc}
       end
 
-    if type do
-      # This is a type hint, if we're raising an error, we tell it what the value
-      # type *would* be in this expression so that we can return a "NULL" of that type
-      # its weird, but there isn't any other way that I can tell :)
-      validate_type!(query, type, value)
-
-      type =
-        parameterized_type(
-          bindings.sql_behaviour,
-          type,
-          [],
-          :expr
-        )
-
+    dynamic_type =
       if type do
-        dynamic = Ecto.Query.dynamic(type(fragment("NULL"), ^type))
+        # This is a type hint, if we're raising an error, we tell it what the value
+        # type *would* be in this expression so that we can return a "NULL" of that type
+        # its weird, but there isn't any other way that I can tell :)
+        validate_type!(query, type, value)
 
-        {Ecto.Query.dynamic(fragment("ash_raise_error(?::jsonb, ?)", ^encoded, ^dynamic)), acc}
+        type =
+          parameterized_type(
+            bindings.sql_behaviour,
+            type,
+            [],
+            :expr
+          )
+
+        Ecto.Query.dynamic(type(fragment("NULL"), ^type))
       else
-        {Ecto.Query.dynamic(fragment("ash_raise_error(?::jsonb)", ^encoded)), acc}
+        nil
       end
-    else
-      {Ecto.Query.dynamic(fragment("ash_raise_error(?::jsonb)", ^encoded)), acc}
+
+    # A row-dependent token for immutable calls, or nil if immutable_expr_error is not opted-in to.
+    row_token = immutable_error_expr_token(query, bindings)
+
+    case {dynamic_type, row_token} do
+      {nil, nil} ->
+        {Ecto.Query.dynamic(fragment("ash_raise_error(?::jsonb)", ^encoded)), acc}
+
+      {dynamic_type, nil} ->
+        {Ecto.Query.dynamic(fragment("ash_raise_error(?::jsonb, ?)", ^encoded, ^dynamic_type)),
+         acc}
+
+      {nil, row_token} ->
+        {Ecto.Query.dynamic(
+           fragment("ash_raise_error_immutable(?::jsonb, ?)", ^encoded, ^row_token)
+         ), acc}
+
+      {dynamic_type, row_token} ->
+        {Ecto.Query.dynamic(
+           fragment(
+             "ash_raise_error_immutable(?::jsonb, ?, ?)",
+             ^encoded,
+             ^dynamic_type,
+             ^row_token
+           )
+         ), acc}
     end
   end
 
@@ -3239,6 +3261,51 @@ defmodule AshSql.Expr do
       {query.__ash_bindings__.sql_behaviour.type_expr(expr, type), acc}
     else
       {expr, acc}
+    end
+  end
+
+  # Returns a row-dependent token to prevent constant-folding for immutable functions, or nil if
+  # the repo hasn't opted-in to immutable expr_errors.
+  #
+  # The expression doesn't matter, as long as it depends on the row to avoid constant-folding.
+  defp immutable_error_expr_token(query, bindings) do
+    resource = query.__ash_bindings__.resource
+    repo_config = query.__ash_bindings__.sql_behaviour.repo(resource, :read).config()
+
+    if Keyword.get(repo_config, :immutable_expr_error?, false) do
+      ref_binding = bindings.root_binding
+
+      pk_attr_names = Ash.Resource.Info.primary_key(resource)
+
+      attr_names =
+        case pk_attr_names do
+          [] ->
+            case Ash.Resource.Info.attributes(resource) do
+              [%{name: name} | _] -> [name]
+              _ -> []
+            end
+
+          pk ->
+            pk
+        end
+
+      if ref_binding && attr_names != [] do
+        Enum.reduce(attr_names, Ecto.Query.dynamic(fragment("''::text")), fn attr_name, acc ->
+          value_expr =
+            if bindings[:parent?] &&
+                 ref_binding not in List.wrap(bindings[:lateral_join_bindings]) do
+              Ecto.Query.dynamic(field(parent_as(^ref_binding), ^attr_name))
+            else
+              Ecto.Query.dynamic(field(as(^ref_binding), ^attr_name))
+            end
+
+          Ecto.Query.dynamic(fragment("? || '|' || (? )::text", ^acc, ^value_expr))
+        end)
+      else
+        nil
+      end
+    else
+      nil
     end
   end
 
